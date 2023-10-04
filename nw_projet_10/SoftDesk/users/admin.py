c05cf8bdedfaa70475f975c64d39b01214b0c5d2
Sami Chelly <samichelly15@gmail.com>
2023-10-04 19:35:11 +0200

    Adding Projects into admin.py + optimizations

--- nw_projet_10/SoftDesk/users/admin.py
+++ nw_projet_10/SoftDesk/users/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+from projects.models import Project
+
 
 class CustomUserAdmin(UserAdmin):
     list_display = (
@@ -30,3 +32,32 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = (
+        "author",
+        "title",
+        "description",
+        "project_type",
+        "created_time",
+        # "contributors",
+    )
+    # list_filter = ("date_of_birth", "can_be_contacted", "can_data_be_shared")
+    # fieldsets = (
+    #     (None, {"fields": ("username", "password")}),
+    #     (
+    #         "Personal info",
+    #         {
+    #             "fields": (
+    #                 "email",
+    #                 "date_of_birth",
+    #                 "can_be_contacted",
+    #                 "can_data_be_shared",
+    #             )
+    #         },
+    #     ),
+    # )
+
+
+admin.site.register(Project, ProjectsAdmin)
