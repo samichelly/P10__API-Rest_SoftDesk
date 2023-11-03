3f4c9d94980b8facac7e8be5b7a9c7bb941013eb
Sami Chelly <samichelly15@gmail.com>
2023-11-03 22:44:10 +0100

    fixing bug delete method

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework import status, generics
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import CustomUser
-from projects.models import Project, Contributor, Issue, Comment
+from projects.models import Project, Contributor, Issue
 from .serializers import SignupSerializer, CustomUserSerializer
 
 
@@ -12,7 +12,6 @@ class SignupView(generics.CreateAPIView):
     """
     View for user signup.
     """
-
     queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = SignupSerializer
@@ -24,14 +23,11 @@ def users_list(request):
     """
     Get the list of users who share their data.
     """
-    # if request.user:
     queryset = CustomUser.objects.all().filter(can_data_be_shared=True)
     current_user = CustomUser.objects.get(pk=request.user.id)
     queryset |= CustomUser.objects.filter(pk=current_user.id)
     serializer = CustomUserSerializer(queryset, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
-    # else:
-    #     return Response("Protected data", status=status.HTTP_403_FORBIDDEN)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -60,13 +56,9 @@ def user_detail(request, user_pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
-        print("test")
         projects_to_reassign = Project.objects.filter(author=user)
         issues_to_reassign = Issue.objects.filter(author=user)
-        print("projects_to_reassign")
-        print(projects_to_reassign)
-        print("issues_to_reassign")
-        print(issues_to_reassign)
+
         new_author = None
         new_issue_author = False
 
@@ -74,8 +66,6 @@ def user_detail(request, user_pk):
             new_author = Contributor.objects.filter(
                 project=project, role="CONTRIBUTOR"
             ).first()
-            print("new_author")
-            print(new_author)
             if new_author:
                 new_author.role = "AUTHOR"
                 new_author.save()
@@ -83,26 +73,22 @@ def user_detail(request, user_pk):
                 project.save()
 
         for issue in issues_to_reassign:
-            print("issue1")
-            # print(new_author)
             if issue.project.author != user:
                 issue.author = issue.project.author
+                print(issue.author)
                 new_issue_author = True
                 issue.save()
             else:
                 if new_author:
-                    print("issue2")
                     issue.author = new_author.user
                     issue.save()
 
         if new_author or new_issue_author:
-            print("gomme")
             user.date_of_birth = None
             user.can_be_contacted = False
             user.can_data_be_shared = False
             user.is_active = False
             user.save()
-            print("gomme OK")
             return Response(
                 "Profile soft deleted, new author assigned",
                 status=status.HTTP_204_NO_CONTENT,
@@ -112,48 +98,3 @@ def user_detail(request, user_pk):
                 "You must define a new contributor before deleting the user",
                 status=status.HTTP_400_BAD_REQUEST,
             )
-
-
-"""
-    elif request.method == "DELETE":
-        try:
-            projects_to_reassign = Project.objects.filter(author=user)
-            for project in projects_to_reassign:
-                new_author = Contributor.objects.filter(
-                    project=project, role="CONTRIBUTOR"
-                ).first()
-
-                if new_author:
-                    new_author.role = "AUTHOR"
-                    new_author.save()
-
-                    project.author = new_author.user
-                    project.save()
-        try:
-            issues_to_reassign = Issue.objects.filter(author=user)
-                issue.author = new_author.user
-                project.save()
-
-            # finally
-                user.username = "data unavailable"
-                user.email = "data unavailable"
-                user.date_of_birth = None
-                user.can_be_contacted = False
-                user.can_data_be_shared = False
-                user.save()
-                return Response(
-                    "Profile soft deleted, new author assigned",
-                    status=status.HTTP_204_NO_CONTENT,
-                )
-
-            else:
-                return Response(
-                    "You must define a new contributor before deleting the user",
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-        # ajouter un user temporaire
-        # ajouter la possibilité d'etre supp par un admin
-        # garder un trace de l'utilisateur
-
-        """
